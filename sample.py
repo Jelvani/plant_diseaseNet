d4fb9d4bbdbadaa339e755bcdc40094ad8671a3c
Jelvani <aj654@rutgers.edu>
2020-12-26 22:52:20 -0500

    added new model, but is not accurate still, maybe I will test Alexnet

--- sample.py
+++ sample.py
@@ -1,6 +1,17 @@
 import train
 
-plant = train.plant_classify()
-train, val = plant.getData()
-model = plant.train(train,val)
-plant.save(model)
+def training():
+    global train
+    plant = train.plant_classify()
+    train, val = plant.getData()
+    model = plant.train(train,val)
+    plant.save(model)
+
+def test():
+    plant = train.plant_classify()
+    plant.getData()
+    model = plant.loadModel('model1.h5')
+    image = plant.loadImage('C:/Users/Admin/upper-leaf-mold-tomato.jpg')
+    print(plant.feed(model,image))
+if __name__ == "__main__":
+    test()
